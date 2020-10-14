72ec0a781fba213cac358c43639475ea7b18bd87
Wajahat Ali <wajahat.mech@gmail.com>
2020-10-15 00:35:15 +0500

    pareto chart

--- pandas/linechart.py
+++ pandas/linechart.py
@@ -1,2 +1,20 @@
-import numpy as np
+
 import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
+
+df = pd.DataFrame({'country': [177.0, 7.0, 4.0, 2.0, 2.0, 1.0, 1.0, 1.0]})
+df.index = ['USA', 'Canada', 'Russia', 'UK', 'Belgium', 'Mexico', 'Germany', 'Denmark']
+df = df.sort_values(by='country',ascending=False)
+df["cumpercentage"] = df["country"].cumsum()/df["country"].sum()*100
+
+
+fig, ax = plt.subplots()
+ax.bar(df.index, df["country"], color="C0")
+ax2 = ax.twinx()
+ax2.plot(df.index, df["cumpercentage"], color="C1", marker="D", ms=7)
+ax2.yaxis.set_major_formatter(PercentFormatter())
+
+ax.tick_params(axis="y", colors="C0")
+ax2.tick_params(axis="y", colors="C1")
+plt.show()
